6953149394a6a895f59706190032ed62c40c1054
KingFarras <askarakawa4@gmail.com>
2024-07-12 11:45:02 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,23 @@
+import streamlit as st
+
+st.write('ini aplikasi dewa :sunglasses:')
+
+import streamlit as st
+
+st.header('This is zeus ', divider='green')
+st.header('_kawa_ is :blue[cool] :sunglasses:')
+
+import streamlit as st
+
+st.markdown("*ilham* is **really** ***cool***.")
+st.markdown('''
+    :red[ilham] :orange[can] :green[walk] :blue[in] :violet[water]
+    :gray[with his] :rainbow[head] and :blue-background[highlight] text.''')
+st.markdown("Here's a bouquet from ilham &mdash;\
+            :tulip::cherry_blossom::rose::hibiscus::sunflower::blossom:")
+
+import streamlit as st
+
+st.subheader('This is a dr.ilham', divider='rainbow')
+st.subheader('_Streamlit_ is :blue[cool] :sunglasses:')
+
